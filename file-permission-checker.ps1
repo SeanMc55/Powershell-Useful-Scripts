f36629220de4e110be4e4cc16255b242f1948a28
SeanMc55 <seanmcmillin3259@gmail.com>
2020-07-10 15:12:17 -0700

    Update file-permission-checker.ps1
    initial work on automation. need to figure out how to bring in the list of paths successfully

--- file-permission-checker.ps1
+++ file-permission-checker.ps1
@@ -1,15 +1,32 @@
-$dirs = Get-ChildItem "X:\Sales\*", "X:\Sales\Projects\*" |
-        where-object {($_.PsIsContainer)}
- $acls = foreach($dir in $dirs)
- {
-     try
-     {
-         Get-Acl $dir -ErrorAction Stop
-     }
-     catch
-     {
-         "Failed to process $dir"
-     }
+# Do {
+#     $inputFound = $true
+#     $inputValue = Read-Host "Enter a directory to search"
+    
+#     If ( $inputValue -eq "" ) {
+#             $inputFound = $false    
+#     }
+#     Else {
+#         [array]$inputList = $inputList + $inputValue
+#     }
+# }
+# While ($inputFound -eq $true)
+
+# $inputList
+
+
+$dirs = Get-ChildItem "X:\*", "X:\*\*", "X:\*\*\*" | where-object {($_.PsIsContainer)}
+# $dirs = Get-ChildItem ('"{0}"' -f $($inputList -join '","')) | where-object {($_.PsIsContainer)}
+
+$acls = foreach($dir in $dirs)
+{
+    try
+    {
+        Get-Acl $dir -ErrorAction Stop
+    }
+    catch
+    {
+        "Failed to process $dir"
+    }
 
 }
-$acls | Out-File "C:\Users\Sean McMillin\Desktop\Permissions.txt"
+$acls | Select-String -Pattern "Failed to process" | Out-File "C:\Users\Sean McMillin\Desktop\All - Permissions.txt"
